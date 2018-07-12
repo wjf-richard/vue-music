7afa2003aaa6521e6476ecdd984a8e2687b78100
wjf <wjf@swk-mac-mini-02.local>
2018-07-12 19:15:03 +0800

    更新vue-music

--- src/common/js/jsonp.js
+++ src/common/js/jsonp.js
@@ -1,20 +1,19 @@
 import originJsonp from 'jsonp'
-
-export default function jsonp(url, data, option){
+export default function jsonp(url, data, option) {
   url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
-  
   return new Promise((resolve, reject) => {
-    if(!err){
-      resolve(data)
-    }else{
-      reject(err)
-    }
+    originJsonp(url, option, (err, data) => {
+      if (!err) {
+        resolve(data)
+      } else {
+        reject(err)
+      }
+    })
   })
 }
-
-export function param(data){
-  let url=''
-  for(var k in data) {
+export function param(data) {
+  let url = ''
+  for (var k in data) {
     let value = data[k] !== undefined ? data[k] : ''
     url += '&' + '=' + encodeURIComponent(value)
   }
