bbe5b8ad453ab51cec1dc2ca9a22a7e570ecc3e5
wjf <wjf@swk-mac-mini-02.local>
2018-11-06 19:00:30 +0800

    歌曲详情页面的VUEX状态

--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,13 @@
+import { palyMode } from '../common/js/config'
+
 const state = {
-  singer: {}
+  singer: {},
+  playing: false,
+  fullScreen: false,
+  playlist: [],
+  sequenceList: [],
+  mode: palyMode.sequence,
+  currentIndex: -1
 }
 
 export default state
